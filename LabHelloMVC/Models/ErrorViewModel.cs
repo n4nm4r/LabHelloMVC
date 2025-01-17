b23d8cadb2ac7dea903063daf55a2e9f1c786db2
n4nm4r <nancy.martin.nb@gmail.com>
2025-01-17 13:58:04 -0400

    Add project files.

---
+++ LabHelloMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabHelloMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LabHelloMVC/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabHelloMVC.Models
+{
+    public class Person : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
